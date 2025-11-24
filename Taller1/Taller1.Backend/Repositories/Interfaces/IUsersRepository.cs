242eec18e78f17d15197a346d1adb6ca26226832
mateomarquez915 <mateomarquez315389@correo.itm.edu.co>
2025-11-24 18:26:00 -0500

    Create edit user

--- Taller1/Taller1.Backend/Repositories/Interfaces/IUsersRepository.cs
+++ Taller1/Taller1.Backend/Repositories/Interfaces/IUsersRepository.cs
@@ -6,6 +6,12 @@ namespace Taller1.Backend.Repositories.Interfaces;
 
 public interface IUsersRepository
 {
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+    Task<IdentityResult> UpdateUserAsync(User user);
+
     Task<SignInResult> LoginAsync(LoginDTO model);
 
     Task LogoutAsync();
--- Taller1/Taller1.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
+++ Taller1/Taller1.Backend/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
@@ -6,6 +6,12 @@ namespace Taller1.Backend.UnitsOfWork.Interfaces;
 
 public interface IUsersUnitOfWork
 {
+    Task<User> GetUserAsync(Guid userId);
+
+    Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+    Task<IdentityResult> UpdateUserAsync(User user);
+
     Task<SignInResult> LoginAsync(LoginDTO model);
 
     Task LogoutAsync();
